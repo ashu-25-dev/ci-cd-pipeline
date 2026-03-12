89a59a9063eed668a115a508ac8c24ea556e054f
Ashmitha Nayana Jain <ashmithanayanajain@gmail.com>
2026-03-12 12:28:02 +0530

    Remove CSE-A heading from App component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>BY ASHMITHA ABHI</h2>
-      <h3>CSE-A</h3>
       <h4>SIT,valachil,mangalore</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
